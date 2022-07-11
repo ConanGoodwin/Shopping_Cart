119a885f864efcee6fbf4af852c33c6c96b4a82e
Conan Goodwin <conan_goodwin@hotmail.com>
2022-07-11 18:59:39 -0300

    Requisito 06

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,5 +1,6 @@
-const saveCartItems = () => {
+const saveCartItems = (carrinho) => {
   // seu código aqui
+  localStorage.setItem('cartItems', carrinho);
 };
 
 if (typeof module !== 'undefined') {
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,15 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se, ao executar saveCartItems com o argumento \'<ol><li>Item</li></ol>\', o método \'localStorage.setItem\' é chamado:', () => {
+    const testeFunc = saveCartItems('<ol><li>Item</li></ol>');
+
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('Testa se, ao executar \'saveCartItems\' com o argumento \'<ol><li>Item</li></ol>\', o método \'localStorage.setItem\' é chamado com dois parâmetros, sendo o primeiro \'cartItems\' e o segundo sendo o valor passado como argumento para saveCartItems:', () => {
+    const testeFunc = saveCartItems('<ol><li>Item</li></ol>');
+
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
 });
